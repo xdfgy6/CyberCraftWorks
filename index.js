cccea538be222905159023f7d43d5e0f64696aab
xdfgy6 <gs1surplexd@hotmail.com>
2024-05-14 12:25:22 +0700

    commit

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-function bubbleSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+function solveNQueens(n) {
+  const result = [];
+  const board = Array.from({ length: n }, () =>
+    Array.from({ length: n }, () => "."),
+  );
+  backtrack(0);
+  return result;
+  function backtrack(row) {
+    if (row === n) {
+      result.push(board.map((row) => row.join("")));
+      return;
+    }
+    for (let col = 0; col < n; col++) {
+      if (isValid(row, col)) {
+        board[row][col] = "Q";
+        backtrack(row + 1);
+        board[row][col] = ".";
       }
     }
   }
-  return arr;
+  function isValid(row, col) {
+    for (let i = 0; i < row; i++) {
+      if (board[i][col] === "Q") return false;
+    }
+    for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+      if (board[i][j] === "Q") return false;
+    }
+    for (let i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+      if (board[i][j] === "Q") return false;
+    }
+    return true;
+  }
 }
